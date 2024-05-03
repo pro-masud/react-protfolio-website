ca492e393b217b59e92d8e8bbbc005011aaa1066
pro-masud <promasudbd@gmail.com>
2024-05-03 11:18:28 +0600

    portflio website navbar menu complete

--- Portfolio Website/src/components/Navbar/Navbar.jsx
+++ Portfolio Website/src/components/Navbar/Navbar.jsx
@@ -2,16 +2,16 @@ import React, {useState} from 'react';
 import style from "./Navbar.module.css";
 import { getImageUrl } from '../../utils/utils.js';
 
-
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={style.navBar}>
       <div className={style.logo}>
         <a href="/">Portfolio</a>
       </div>
       <div className={style.menu}>
-        <img className='menuBar' src={getImageUrl('nav/nav.png')} />
-        <ul className={style.menuItems}>
+        <img onClick={ () => setMenuOpen(!menuOpen)} className={style.menuBar} src={ menuOpen ? getImageUrl('nav/close-menu.png') : getImageUrl('nav/nav.png')} />
+        <ul className={`${style.menuItems} ${menuOpen && style.menuOpen}`}>
           <li><a href="#About">About Us</a></li>
           <li><a href="#Portfolio">Portfolio</a></li>
           <li><a href="#Project">Project</a></li>
